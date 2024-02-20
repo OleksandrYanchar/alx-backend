8372e8ba277704f49e6787322525f292e992bc7c
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-20 04:35:51 +0200

    Init: get session dependence

---
+++ app/dependencies/db.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from db.db import engine
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
+
