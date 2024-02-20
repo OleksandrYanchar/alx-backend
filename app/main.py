797ae2b888f39a8fd7ff55cb0215202b4798d419
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-20 04:34:36 +0200

    Init: main executing file

---
+++ app/main.py
@@ -0,0 +1,26 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import UJSONResponse
+from api.v1.routers import router as v1_router
+from fastapi import APIRouter
+
+app = FastAPI(
+    title="FastAPI Starter Project",
+    description="FastAPI Starter Project",
+    version="1.0",
+    docs_url="/api/docs/",
+    redoc_url="/api/redoc/",
+    openapi_url="/api/openapi.json",
+    default_response_class=UJSONResponse,
+)
+router = APIRouter(
+    prefix="/api",
+    tags=["api"],
+)
+router.include_router(v1_router)
+
+app.include_router(router)
+
+
+if __name__ == "__main__":
+        uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
