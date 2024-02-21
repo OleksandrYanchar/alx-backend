10f0bf662724e54f81724ae197c9d22ef42ce738
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-21 03:17:35 +0200

    ADD: db setup

--- app/db/db.py
+++ app/db/db.py
@@ -1,15 +1,6 @@
 from sqlalchemy.ext.asyncio import  create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DB_NAME = os.getenv('DB_NAME')
-DB_PASS = os.getenv('DB_PASS')
-DB_USER = os.getenv('DB_USER')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+from configs.db import DB_HOST,DB_NAME, DB_PASS, DB_PORT, DB_USER 
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
