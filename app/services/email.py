268702059578074d20137c2e7d6d36553d5cdb3a
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-22 08:02:01 +0200

    UPDATE: minnor changes

--- app/services/email.py
+++ app/services/email.py
@@ -1,6 +1,6 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 import jwt
-from configs.auth import SECRET
+from configs.auth import ALGORITHM, SECRET
 from configs.emails import EMAIL_HOST_PASSWORD, EMAIL_HOST_USER, EMAIL_HOST_USERNAME
 from models.users import Users
 from schemas.email import EmailSchema
@@ -18,34 +18,29 @@ config = ConnectionConfig (
     VALIDATE_CERTS = True
 )
 
-async def send_email(email: EmailSchema, instance: Users):
-    # Safely ferrying the 'id' over by shifting it to the terminal of a 'str', 
-    # to foot the language that JSON encodes.
+async def send_email(email: EmailSchema, user: Users):
     token_data = {
-        'id': str(instance.id),  # Taming the ID for a space through the extent of a JSON
-        'username': instance.username,
+        'user_id': str(user.id),  # Adjusting to match the verify_token expectations
+        'username': user.username,
     }
     
-    # Refer: Nothing on the tier would stir, the length and the peal, a hamper too neat for a wrangle.
-    token = jwt.encode(token_data, SECRET, algorithm='HS256')
+    token = jwt.encode(token_data, SECRET, algorithm=ALGORITHM)
 
-    # Down the menial and your gesture to their steer, things would sing the sterling path they did.
     template_path = Path(__file__).parent.parent / 'templates/send-verivication.html'
     with open(template_path, 'r') as f:
         template = f.read()
 
-    # The same dimple, your delves for the brink, the wheel that drives the mark, remains the sketch at the pull.
-    template = template.replace(
-        "{{token}}", token.decode('utf-8') if isinstance(token, bytes) else token)
+    template = template.replace("{{token}}", token)
 
-    # O, Marrow to the coach, your gesture, the craft, the rope, they're up in the house, they’re a bedel at the sign.
+    # Assuming email_schema.email is a list and you want to send email to the first address in the list
+
     message = MessageSchema(
         subject='Email Verification',
-        recipients=email,  # Care the lost flick, it's a tenfold of a cast, not the hay of the slip.
+        recipients=email,  # This is now a list, as expected by MessageSchema
         body=template,
         subtype='html',
     )
 
-    # We fasten our cithern, we've given our boast, as you cue it, it hails, the app's docked, it’s sent for the fair.
     fm = FastMail(config)
-    await fm.send_message(message=message)
+    print(token)
+    await fm.send_message(message)
