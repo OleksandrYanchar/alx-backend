f2707133e39ff43a00a6e32b4330bb84f311d304
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-22 07:57:44 +0200

    UPDAET: added verify user credentials and removed token verification

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,11 +1,9 @@
-from  datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
 from passlib.context import CryptContext
-from schemas.users import DataTokenSchema
-from configs.auth import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET
-from jose import JWTError, jwt
-import jwt
-
-
+from dependencies.db import get_async_session
+from models.users import Users
+from sqlalchemy.ext.asyncio import AsyncSession
+from utils.objects import get_object
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -16,28 +14,9 @@ def get_password_hash(password):
 def verify_password(plain_password, hashed_pass):
     return pwd_context.verify(plain_password, hashed_pass)
 
-async def verify_token_access(token: str, credentials_exception):
-    try:
-        payload = jwt.decode(token, SECRET, algorithms=['HS256'])
-        
-        # Ensure this matches the key used in the JWT payload
-        user_id: str = payload.get("id")  
-        if user_id is None:
-            raise credentials_exception
-        # Assuming DataTokenSchema is defined elsewhere and correctly handles the 'id'
-        token_data = DataTokenSchema(id=user_id)
-    except JWTError as e:
-        print(e)
-        raise credentials_exception
 
-    return token_data
-
-
-async def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    # Ensure the 'id' and 'joined_at' are properly converted to strings
-    to_encode.update({"exp": expire, "id": str(data["id"]), "joined_at": data["joined_at"].isoformat()})
-    
-    encoded_jwt = jwt.encode(to_encode, SECRET, algorithm='HS256')
-    return encoded_jwt
+async def verify_user_credentials(username: str, password: str, session: AsyncSession = Depends(get_async_session)) -> Users:
+    user = await get_object(Users,session=session, username=username)
+    if not user or not pwd_context.verify(password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    return user
