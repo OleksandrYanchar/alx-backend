f001e65a3aa04d62ae541c2931c9e4f146191b75
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-23 06:14:59 +0200

    UPDATE:added refresh token schema

--- app/schemas/tokens.py
+++ app/schemas/tokens.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, UUID4
 # if needed for access vs. refresh token purposes.
 class TokenPayload(BaseModel):
     user_id: str
-    username: str
+    username:  Optional[str] = None
     is_activated: Optional[bool] = None
     is_staff: Optional[bool] = None
 
@@ -18,3 +18,6 @@ class Token(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+    
+class TokenRefreshRequest(BaseModel):
+    refresh_token: str
