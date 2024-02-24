fb4dbeab666c84e3f5506e52905f494432f29849
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-24 12:27:25 +0200

    UPDATE: fixed routing

--- app/api/v1/users/routers.py
+++ app/api/v1/users/routers.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
-from api.v1.users.auth import router as user_router
- 
+from api.v1.users.auth import router as auth_router
+
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
 
-
-router.include_router(user_router) 
+router.include_router(auth_router) 
