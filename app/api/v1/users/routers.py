b2f0e0748a372c29d5b0434a68ec94d00e12a852
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-25 08:22:36 +0200

    UPDATE: vhanged routing

--- app/api/v1/users/routers.py
+++ app/api/v1/users/routers.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from api.v1.users.auth import router as auth_router
-
+from api.v1.users.profile import router as profile_router
 router = APIRouter(
     prefix="/users",
     tags=["users"],
 )
 
-router.include_router(auth_router)
+
+router.include_router(auth_router) 
+router.include_router(profile_router) 
