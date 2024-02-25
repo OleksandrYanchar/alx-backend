2730762389274f389ef55a55f6f5b7aae97449da
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-25 08:21:56 +0200

    ADD: update profile image

---
+++ app/api/v1/users/profile.py
@@ -0,0 +1,66 @@
+
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+import secrets
+from models.users import Users
+from dependencies.db import get_async_session
+from dependencies.auth import get_current_user
+from configs.general import  MEDIA_FILES_PATH
+from PIL import Image
+from sqlalchemy.ext.asyncio import AsyncSession
+import aiofiles
+
+router = APIRouter(
+    prefix="/profile",
+    tags=["profile"],
+)
+
+
+@router.post('/update-image/')
+async def update_photo(file: UploadFile = File(...), user: Users = Depends(get_current_user), db: AsyncSession = Depends(get_async_session)):
+
+    """
+    Update the image for a specific user.
+    """
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    filename = file.filename
+    extension = filename.split(".")[-1].lower()
+    if extension not in ["jpg", "png"]:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File extension must be .jpg or .png")
+
+    # Here you would save the file in a directory or cloud storage
+    # For simplicity, let's assume you're saving the file locally
+    token_name = secrets.token_hex(10) + "." + extension
+    generated_name = MEDIA_FILES_PATH + f'avatars/{token_name}'
+    file_url = generated_name.split('static/')[1]
+
+    # Write the file asynchronously
+    try:
+        file_content = await file.read()
+        async with aiofiles.open(generated_name, "wb") as out_file:
+            await out_file.write(file_content)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+    # Process the image
+    try:
+        with Image.open(generated_name) as img:
+            img = img.resize(size=(512, 512))
+            img.save(generated_name)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unable to process image")
+
+    # Update the user record
+    try:
+        user.image = file_url
+        await db.commit()
+        await db.refresh(user)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Unable to save user info")
+
+    return {"status": "success", 
+            "detail": "Image uploaded successfully.",
+            'image': file_url}
+            
+            
