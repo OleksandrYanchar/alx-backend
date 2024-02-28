d46507a7806c82c1ae1085f203b0e8255cde37fa
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-02-28 02:37:41 +0200

    UPDATE: password forgot schema

--- app/schemas/email.py
+++ app/schemas/email.py
@@ -1,12 +1,5 @@
 from pydantic import BaseModel
-from typing import List
 
-
-class EmailSchema(BaseModel):
-    email: List[str]
-    subject: str
-    body: str
-
-class EmailVerificationSchema(BaseModel):
-    username: str
-    is_active: bool
+class ForgotPasswordEmailSchema(BaseModel): 
+    identifier: str
+    
