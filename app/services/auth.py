666ba3e7ad6c37003c83b71f06ecc2d2bd00bc59
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-01 00:01:59 +0200

    UPDATE: refactored

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,9 +1,9 @@
 from fastapi import Depends, HTTPException, status
 from passlib.context import CryptContext
+from crud.users import crud_user
 from dependencies.db import get_async_session
 from models.users import Users
 from sqlalchemy.ext.asyncio import AsyncSession
-from utils.objects import get_object
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -19,13 +19,21 @@ def verify_password(plain_password, hashed_pass):
 async def verify_user_credentials(
     username: str, password: str, session: AsyncSession = Depends(get_async_session)
 ) -> Users:
-    try:
-        user = await get_object(Users, session=session, username=username)
-    except Exception:
-        user = await get_object(Users, session=session, email=username)
+    user = await crud_user.get(session, username=username)
+    
+    if not user:
+        user = await crud_user.get(session, email=username)
+    
+    
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="user with this email or username dosent exist",
+        )
+         
     
     # If user is still not found or password does not match, then raise an exception
-    if not user or not pwd_context.verify(password, user.password):
+    if not pwd_context.verify(password, user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
