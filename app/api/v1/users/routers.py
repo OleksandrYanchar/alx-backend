ed12173652f940c73ef9f993f9ec0c8c3a5e476f
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-02 13:20:05 +0200

    UPDATE: added profile router

--- app/api/v1/users/routers.py
+++ app/api/v1/users/routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from api.v1.users.auth import router as auth_router
+from api.v1.users.profile import router as profiles_router
 
 router = APIRouter(
     prefix="/users",
@@ -7,3 +8,4 @@ router = APIRouter(
 )
 
 router.include_router(auth_router)
+router.include_router(profiles_router)
