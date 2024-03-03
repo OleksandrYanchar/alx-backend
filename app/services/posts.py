35c52a73269ceb9aa9a998733991b83b2faa7acf
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-03 17:32:24 +0200

    REFACTOR

--- app/services/posts.py
+++ app/services/posts.py
@@ -15,8 +15,11 @@ async def create_slug(title: str) -> str:
     # Ensure to call .lower() to convert the title to lowercase
     return title.strip().replace(' ', '-').lower()
 
+async def is_subcategory_in_category(db: AsyncSession, category_id: int, sub_category_id: int):
+    return await crud_subcategory.get(db, id=sub_category_id, category_id=category_id)
 
-async def create_category(
+
+async def perfome_create_category(
     category_data: CreateCategorySchema, 
     db: AsyncSession,
 ) -> CategoryInfoSchema:
@@ -57,13 +60,12 @@ async def create_category(
     return CategoryInfoSchema(**new_category.dict())
 
 
-async def create_subcategory(
+async def perfome_create_subcategory(
     subcategory_data: CreateSubCategorySchema, 
     db: AsyncSession,
 ) -> SubCategoryCrateInfoSchema:
     title = await clean_title(subcategory_data.title)
     slug = await create_slug(title)
-    category = await crud_category.get(db, title=subcategory_data.category)
     
     if len(title) >64:
         raise HTTPException(
@@ -88,29 +90,21 @@ async def create_subcategory(
             detail="this slug already exists.",
         )
     
-    if not category:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="This category dosen't exists.",
-        )
+    
+    category = await crud_category.get(db, title= await clean_title(subcategory_data.category))
 
-    subcategory_data_dict = subcategory_data.dict()
+    # Prepare the data for subcategory creation
+    subcategory_data_dict = subcategory_data.dict(exclude={'category'})
     subcategory_data_dict['title'] = title
-    subcategory_data_dict['category_id'] = category.id  # Set the correct 'category_id'
+    subcategory_data_dict['category_id'] = category.id  # use the retrieved category's ID
     subcategory_data_dict['slug'] = slug
-
-    # Remove the 'category' key which is not needed for creating the SubCategory object
-    subcategory_data_dict.pop('category', None)
 
     # Create the new SubCategory using the 'subcategory_data_dict'
     new_subcategory = await crud_subcategory.create(db, obj_in=subcategory_data_dict)
 
-    # Fetch the category name for including in the SubCategoryInfoSchema
-    category_name = category.title
-
-    # Create the SubCategoryInfoSchema, adding the category name
+    # Construct the SubCategoryInfoSchema object with the category name included
     subcategory_info_data = new_subcategory.dict()
-    subcategory_info_data['category'] = category_name
+    subcategory_info_data['category'] = subcategory_data.category  # use the retrieved category's title
 
     # Return the SubCategoryInfoSchema instance
     return SubCategoryCrateInfoSchema(**subcategory_info_data)
