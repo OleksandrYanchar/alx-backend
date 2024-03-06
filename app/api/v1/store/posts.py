6aa1812d0f7f5c1b84031b37fab1d13f19bbb3c8
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-06 15:29:50 +0200

    UPDATE: added post by id endpoint

--- app/api/v1/store/posts.py
+++ app/api/v1/store/posts.py
@@ -110,7 +110,7 @@ async def get_posts(
     if subcategory:
         subcategory_obj = await crud_subcategory.get(db, title=subcategory)
         if not subcategory_obj:
-                         detail = "Subategory not found"
+            detail = "Subategory not found"
         subcategory = subcategory_obj.id if subcategory_obj else None
 
     if owner:
@@ -138,7 +138,6 @@ async def get_posts(
         created_at_field_name = 'created_at',  
         vip_field_name = 'is_vip', 
         price_field_name = 'price',
-        id_field_name='id',
         title_field_name='title',
         category_field_name='category_id',
         subcategory_field_name='sub_category_id',
@@ -167,3 +166,96 @@ async def get_posts(
 
 
     return PaginationSchema[PostInfoSchema](total=total, items=result_posts, offset=offset, limit=limit, detail=detail) 
+
+
+@router.get("/id/{id}", response_model=PostInfoSchema)
+async def get_post(id: str, db: AsyncSession=Depends(get_async_session)) -> PostInfoSchema:
+    
+    post = await crud_post.get(db, id=id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    # Assuming `crud_post.get` fetches the post with relational fields loaded,
+    # or you have separate methods to fetch category and subcategory details.
+    owner = await crud_user.get(db, id=post.owner)
+    if not owner:
+        raise HTTPException(status_code=404, detail="Owner not found")
+
+    # Fetch category and subcategory information if not already included in `post`
+    category = await crud_category.get(db, id=post.category_id)
+    subcategory = await crud_subcategory.get(db, id=post.sub_category_id)
+
+    if not category or not subcategory:
+        raise HTTPException(status_code=404, detail="Category or Subcategory not found")
+
+    post_data = post.dict()
+    
+    if 'owner' in post_data:
+        del post_data['owner']
+
+    # Add category and subcategory titles to post_data
+    post_data['category'] = category.title if category else None
+    post_data['subcategory'] = subcategory.title if subcategory else None
+
+    return PostInfoSchema(**post_data, owner=UserDataSchema(**owner.dict()))
+
+
+
+@router.get("/user/{username}/all", response_model=PaginationSchema[PostInfoSchema])
+async def get_posts_by_username(username: str, 
+    offset: int = Query(default=0),  
+    limit: int = Query(default=2), 
+    order_by: str = None,
+    created_start_date: Optional[date] = None,
+    created_end_date: Optional[date] = None,
+    is_vip: Optional[bool] = None,
+    min_price: Optional[float] = None,
+    max_price: Optional[float] = None,
+    db: AsyncSession = Depends(get_async_session)
+) -> PaginationSchema[PostInfoSchema]:
+    
+    owner = await crud_user.get(db, username=username)
+    if not owner:
+        raise HTTPException(status_code=404, detail="Owner not found")
+    posts, total = await crud_post.get_multi_filtered(
+        db, 
+        offset=offset, 
+        limit=limit,
+        is_vip=is_vip,
+        min_price=min_price,
+        max_price=max_price,
+        id=id,
+        category=category,
+        subcategory=subcategory,
+        owner=owner,
+        created_start_date=created_start_date,
+        created_end_date=created_end_date,
+        order_by=order_by,
+        created_at_field_name = 'created_at',  
+        vip_field_name = 'is_vip', 
+        price_field_name = 'price',
+        category_field_name='category_id',
+        subcategory_field_name='sub_category_id',
+        owner_field_name='owner',
+    )
+    
+    
+    result_posts = []
+    for post in posts:
+        category = await crud_category.get(db, id=post.category_id)
+        subcategory = await crud_subcategory.get(db, id=post.sub_category_id)
+        
+        post_data = post.dict()
+        if 'owner' in post_data:
+            del post_data['owner']
+        
+        post_data['category'] = category.title if category else "Category not found"
+        post_data['subcategory'] = subcategory.title if subcategory else "Subcategory not found"
+        
+        owner_data = UserDataSchema(**owner.dict())
+        post_info = PostInfoSchema(**post_data, owner=owner_data)
+                
+        result_posts.append(post_info)
+
+        
+    return PaginationSchema[PostInfoSchema](total=total, items=result_posts, offset=offset, limit=limit)
