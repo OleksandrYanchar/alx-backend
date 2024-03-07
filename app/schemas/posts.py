3c95963d45eda07fe3ce1215a4105e052c5132e9
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-07 15:32:36 +0200

    UPDATE: added price valitadion

--- app/schemas/posts.py
+++ app/schemas/posts.py
@@ -1,5 +1,5 @@
 import uuid
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, validator, Field
 from datetime import datetime
 from typing import Optional
 
@@ -14,7 +14,8 @@ class PostCreateInSchema(BaseModel):
     description: Optional[str] = None
 
     class Config:
-        from_attributes = True  # Assuming you are using this model with SQLAlchemy
+        from_attributes = True
+
 
 class PostInfoSchema(BaseModel):
     id: uuid.UUID
@@ -34,16 +35,22 @@ class PostInfoSchema(BaseModel):
 
 
 class PostUpdateSchema(BaseModel):
-
-    title: str
-    slug:str
-    category: str
-    subcategory: str
-    price: float
+    
+    title: Optional[str]= None
+    slug:Optional[str]= None
+    category: Optional[str]= None
+    subcategory: Optional[str]= None
+    price: Optional[float]= None
     description: Optional[str] = None
     updated_at: datetime = Field(
         default_factory=datetime.now, description="Post updating time"
     )
+
+    @validator('price')
+    def validate_price(cls, value):
+        if value < 0:
+            raise ValueError("price can't be negative")
+        return value
 
     class Config:
         from_attributes = True  # Assuming you are using this model with SQLAlchemy
