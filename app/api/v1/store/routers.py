e6a4de582b9e6c9dc4f1720d6985996052c9c7e4
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-24 01:00:37 +0200

    UPDATE: added admin router

--- app/api/v1/store/routers.py
+++ app/api/v1/store/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from api.v1.store.posts import router as posts_router
 from api.v1.store.categories import router as categories_router
+from api.v1.store.admin import router as admin_router
 
 router = APIRouter(
     prefix="/store",
@@ -9,3 +10,4 @@ router = APIRouter(
 
 router.include_router(posts_router)
 router.include_router(categories_router)
+router.include_router(admin_router)
