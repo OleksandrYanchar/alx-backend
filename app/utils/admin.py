ae329e5af2bc3df4b4788cc67d3b0f6eaac29b29
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-25 22:16:38 +0200

    REFACTOR: minnor changes

--- app/utils/admin.py
+++ app/utils/admin.py
@@ -8,16 +8,16 @@ async def calculate_percentage_change(old_value, new_value):
     old_value = int(old_value)
     
     if new_value == 0 and old_value == 0:
-        return "0.0"  # If both values are zero, the percentage change is 0%
+        return "same"  # If both values are zero, the percentage change is 0%
     elif new_value == 0:
-        return "-100.0"  # If the new value is zero, it's a 100% decrease
+        return "-100%"  # If the new value is zero, it's a 100% decrease
     elif old_value == 0:
-        return "Infinity"  # If the old value is zero, it's an infinite increase percentage-wise
+        return f'+{new_value}'  # If the old value is zero, it's an infinite increase percentage-wise
     elif old_value > new_value:
         percentage_change = new_value / old_value *-100
     else:
         percentage_change = new_value / old_value *100
-    return f"{round(percentage_change, 2)}"
+    return f"{round(percentage_change, 2)}%"
 
 
 
