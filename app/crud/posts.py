6afeab23a43930a533ea325f40b67b66b08dc137
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-26 19:11:30 +0200

    UPDATE: added postimage class

--- app/crud/posts.py
+++ app/crud/posts.py
@@ -1,7 +1,10 @@
 from crud.base import CRUDBase
-from models.posts import Post 
-from schemas.posts import PostCreateInSchema, PostUpdateSchema
+from models.posts import Post, PostImage
+from schemas.posts import PostCreateInSchema, PostImageUpdate, PostUpdateSchema
 
 CRUDPost = CRUDBase[Post, PostCreateInSchema, PostUpdateSchema]
 crud_post = CRUDPost(Post)
+
+CRUDPostImage = CRUDBase[PostImage, PostImageUpdate, PostImageUpdate]
+crud_postimage = CRUDPost(PostImage)
 
