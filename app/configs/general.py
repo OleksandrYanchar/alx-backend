b9c90e4727f1542c8e1e76b40fc28080c5a5d8e6
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-26 19:12:01 +0200

    UPDATE: added avatars and posts dirs urls

--- app/configs/general.py
+++ app/configs/general.py
@@ -5,7 +5,10 @@ import os
 load_dotenv()
 
 STATIC_FILES_PATH = os.getenv('STATIC_FILES_PATH') 
-MEDIA_FILES_PATH = STATIC_FILES_PATH + '/media/'
+MEDIA_FILES_PATH = STATIC_FILES_PATH + 'media/'
+
+AVATARS_DIR = f'{MEDIA_FILES_PATH}avatars/'
+POSTS_PICTURES_DIR = f'{MEDIA_FILES_PATH}posts/'
 
 ADMINS_EMAILS: str = os.getenv('ADMINS_EMAILS')
 
