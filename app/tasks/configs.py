6776576d76153fc10ae14bfc3ba485315af1fe4c
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-26 19:10:08 +0200

    UPDATE: added upload picture celery task

--- app/tasks/configs.py
+++ app/tasks/configs.py
@@ -1,7 +1,5 @@
 from celery import Celery
 from celery.schedules import crontab
-import sys
-print(sys.path)
 
 from configs.db import REDIS_PORT, REDIS_HOST
 celery_app = Celery(
@@ -14,12 +12,13 @@ celery_app = Celery(
 celery_app.conf.update(timezone="UTC")
 
 # In your Celery app initialization module, ensure task discovery is correctly set up
-celery_app.autodiscover_tasks(['tasks'], force=True)
+celery_app.autodiscover_tasks(['tasks.admin'], force=True)
+celery_app.autodiscover_tasks(['tasks.store'], force=True)
 
 
 celery_app.conf.beat_schedule = {
     "generate_daily_report": {
-        "task": "tasks.tasks.generate_daily_report",
+        "task": "generate_report",
         "schedule": crontab(hour=8, minute=0), 
     },
 }
