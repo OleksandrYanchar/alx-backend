6a520e77dbdd6b60eb9ce07b6483047fa8a88ef6
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-28 00:42:39 +0200

    UPDATE: added general store roter

--- app/api/v1/store/routers.py
+++ app/api/v1/store/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from api.v1.store.posts import router as posts_router
 from api.v1.store.categories import router as categories_router
+from api.v1.store.reports import router as repors_router
+
 
 router = APIRouter(
     prefix="/store",
@@ -9,3 +11,4 @@ router = APIRouter(
 
 router.include_router(posts_router)
 router.include_router(categories_router)
+router.include_router(repors_router)
