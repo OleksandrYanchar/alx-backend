b7c49cdcd5241b1e2ddc3a965fa1125f0d7160d4
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-03-29 00:51:11 +0200

    ADD: bug reports and their comments models

--- app/models/store.py
+++ app/models/store.py
@@ -1,5 +1,6 @@
+from typing import Optional, List
 from sqlalchemy import String, ForeignKey, Integer, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 import uuid
 from db.db import Base
 from datetime import datetime
@@ -17,3 +18,19 @@ class BugReport(Base):
     time_stamp: Mapped[datetime] = mapped_column(nullable=False, default=datetime.now)
     is_closed: Mapped[Boolean] = mapped_column(Boolean, default=False)
     is_vip: Mapped[Boolean] = mapped_column(Boolean, default=False)
+
+    closed_by: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    comments: Mapped[List["BugReportComment"]] = relationship("BugReportComment", back_populates="bug_report")
+
+class BugReportComment(Base):
+    __tablename__ = "bugreportcomments"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    bug_report_id: Mapped[int] = mapped_column(ForeignKey('bugreports.id', ondelete="CASCADE"))
+    body: Mapped[str] = mapped_column(String)
+    time_stamp: Mapped[datetime] = mapped_column(nullable=False, default=datetime.today())
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+
+    # This relationship links comments to users
+    user: Mapped["Users"] = relationship("Users", back_populates="comments")
+    bug_report: Mapped["BugReport"] = relationship("BugReport", back_populates="comments")
