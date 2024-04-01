30703c781712e3176a11d8a44b184d222f3f2786
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-04-01 09:43:09 +0300

    UPDATE

--- app/models/users.py
+++ app/models/users.py
@@ -1,5 +1,4 @@
-from typing import List
-from sqlalchemy import String, UUID
+from sqlalchemy import Boolean, Date, String, UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 import uuid
 
@@ -14,20 +13,14 @@ class Users(Base):
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
     username: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
     first_name: Mapped[str] = mapped_column(String(64), nullable=False)
     last_name: Mapped[str] = mapped_column(String(64), nullable=True)
     email: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
-    joined_at: Mapped[date] = mapped_column(nullable=False, default=date.today())
-    is_activated: Mapped[bool] = mapped_column(nullable=False, default=False)
-    is_vip: Mapped[bool] = mapped_column(nullable=False, default=False)
-    viped_at: Mapped[date] = mapped_column(nullable=True)
-    is_staff: Mapped[bool] = mapped_column(nullable=False, default=False)
-    image: Mapped[str] = mapped_column(
-        nullable=False, default="media/avatars/no_avatar.jpg"
-    )
-
-    @property
-    def comments(self):
-        from models.store import BugReportComment
-        return relationship(BugReportComment, back_populates="user")
+    joined_at: Mapped[date] = mapped_column(Date, nullable=False, default=date.today())
+    is_activated: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    is_vip: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    viped_at: Mapped[date] = mapped_column(Date, nullable=True)
+    is_staff: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    image: Mapped[str] = mapped_column(String, nullable=False, default="media/avatars/no_avatar.jpg")
+    comments = relationship("BugReportComment", back_populates="user")
