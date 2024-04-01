98ddedae7520230a23d42c6f8c60336532ffabf4
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-04-01 11:08:43 +0300

    UPDATE: added new tast to recalculate posts vip

--- app/tasks/store.py
+++ app/tasks/store.py
@@ -1,7 +1,13 @@
+import asyncio
+import logging
 import os
 import secrets
+import traceback
 from PIL import Image
+from tasks.admin import AsyncSessionFactory
 from tasks.configs import celery_app
+from crud.posts import crud_post
+from crud.users import crud_user
 
 
 @celery_app.task
@@ -31,3 +37,25 @@ def upload_picture(file_content: bytes, filename: str, dir: str):
     # Assume all paths involve 'static/' for simplicity
     file_url = generated_name.split("static/")[1]
     return file_url
+
+
+@celery_app.task(name='update_post_vip_from_user_vip')
+def update_product_vip_task():
+    async def update_product_vip():
+        async with AsyncSessionFactory() as session:
+            try:
+                users = await crud_user.get_multi(session)
+                
+                for user in users:
+                    posts = await crud_post.get_multi_filtered(session, owner=user.id)
+                    
+                    for post in posts:
+                        await crud_post.update(session, obj_in={'is_vip': user.is_vip})
+            except Exception as e:
+                raise e
+            finally:
+                await session.close()  # Ensure the session is closed
+    try:
+        asyncio.run(update_product_vip())
+    except Exception as e:
+        logging.error(f"Error generating daily report: {e}\n{traceback.format_exc()}")
